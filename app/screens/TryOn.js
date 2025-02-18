735636e966dfe6723c8fb524b959781307cd220b
Darshana Deshpande <Ddeshpande@PK-SWE-MAC-176.local>
2025-02-18 16:47:30 +0530

    landmarks detection done but inverted

--- app/screens/TryOn.js
+++ app/screens/TryOn.js
@@ -3,8 +3,21 @@ import {
   getCameraDevice,
   runAsync,
   useCameraDevice,
+  useCameraPermission,
   useFrameProcessor
 } from 'react-native-vision-camera'
+import {Canvas, Line, vec} from '@shopify/react-native-skia';
+import {
+  Delegate,
+  Dims,
+  MediapipeCamera,
+  Point,
+  RunningMode,
+  denormalizePoint,
+  faceLandmarkDetectionModuleConstants,
+  framePointToView,
+  useFaceLandmarkDetection,
+} from 'react-native-mediapipe';
 import {
   Face,
   FaceDetectionOptions,
@@ -12,6 +25,7 @@ import {
 } from 'react-native-vision-camera-face-detector'
 import {
     Image,
+    SafeAreaView,
     StyleSheet,
     Text,
     TouchableOpacity,
@@ -21,12 +35,47 @@ import React, {useEffect, useRef, useState} from'react';
 
 import { Worklets } from 'react-native-worklets-core'
 
+// this code converts each segment from the "normalized" coordinate space
+// that the face landmarks are in (0-1) to the "view" coordinate space
+export function convertToViewSpace(
+  segment,
+  frameSize,
+  viewSize,
+  mirrored,
+) {
+  return {
+    startPoint: framePointToView(
+      denormalizePoint(segment.startPoint, frameSize),
+      frameSize,
+      viewSize,
+      'cover',
+      mirrored,
+    ),
+    endPoint: framePointToView(
+      denormalizePoint(segment.endPoint, frameSize),
+      frameSize,
+      viewSize,
+      'cover',
+      mirrored,
+    ),
+  };
+}
+
+const landmarks =  faceLandmarkDetectionModuleConstants()?.knownLandmarks
+
+const eyeLandmarks = {
+  // left: faceLandmarkDetectionModuleConstants().knownLandmarks.leftEye,
+  // right: faceLandmarkDetectionModuleConstants().knownLandmarks.rightEye,
+  left: faceLandmarkDetectionModuleConstants().knownLandmarks.leftEyebrow,
+  right: faceLandmarkDetectionModuleConstants().knownLandmarks.rightEyebrow,
+};
+
 export default Homepage = () => {
 
     const camera = useRef(null);
     const devices = Camera.getAvailableCameraDevices()
     const device = getCameraDevice(devices, 'front')
-  
+    const cameraPermission = useCameraPermission();
     const [showCamera, setShowCamera] = useState(true);
     const [imageSource, setImageSource] = useState('');
 
@@ -34,7 +83,9 @@ export default Homepage = () => {
       // detection options
     } ).current
     const { detectFaces } = useFaceDetector( faceDetectionOptions )
-
+    const [segments, setSegments] = useState([
+      {startPoint: {x: 0, y: 0}, endPoint: {x: 100, y: 100}},
+    ]);
   
   
     useEffect(() => {
@@ -44,6 +95,53 @@ export default Homepage = () => {
       }
       getPermission();
     }, []);
+
+    const faceDetection = useFaceLandmarkDetection(
+      (results, viewSize, mirrored) => {
+        const landmarks = results.results[0].faceLandmarks[0];
+        if (!landmarks || landmarks.length === 0) {
+          return;
+        }
+        const frameSize = {
+          width: results.inputImageWidth,
+          height: results.inputImageHeight,
+        };
+        console.log('Results --> ', results.results[0].faceLandmarks[0])
+        // get all the segments for the eyes
+        const leftEyeSegments = eyeLandmarks.left.map(seg =>
+          convertToViewSpace(
+            {
+              startPoint: landmarks[seg.start],
+              endPoint: landmarks[seg.end],
+            },
+            frameSize,
+            viewSize,
+            mirrored,
+          )
+        );
+        const rightEyeSegments = eyeLandmarks.right.map(seg =>
+          convertToViewSpace(
+            {
+              startPoint: landmarks[seg.start],
+              endPoint: landmarks[seg.end],
+            },
+            frameSize,
+            viewSize,
+            mirrored,
+          ),
+        );
+        setSegments([leftEyeSegments, rightEyeSegments].flat());
+      },
+      error => {
+        console.error(`onError: ${error}`);
+      },
+      RunningMode.LIVE_STREAM,
+      'face_landmarker.task',
+      {
+        delegate: Delegate.GPU,
+      },
+    );
+  
   
     const capturePhoto = async () => {
       if (camera.current !== null) {
@@ -57,7 +155,7 @@ export default Homepage = () => {
     if (device == null) {
       return <Text>Camera not available</Text>;
     }
-// -----------------------------
+
     const handleDetectedFaces = Worklets.createRunOnJS( face => { 
       console.log( 'handle detected face', face )
     })
@@ -69,142 +167,98 @@ export default Homepage = () => {
         console.log('faces detected', faces)
     }, [])
 
-
-    return (
+    const RequestPermissions= ({hasCameraPermission, requestCameraPermission}) => {
+      return (
         <View style={styles.container}>
-          {showCamera ? (
-            <>
-              <Camera
-                ref={camera}
-                style={StyleSheet.absoluteFill}
-                device={device}
-                isActive={showCamera}
-                photo={true}
-                outputOrientation='portrait'
-                frameProcessor={frameProcessor}
-                
-                // faceDetectionCallback={(arg)=>{console.log(arg)}}
-              />
-    
-              <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                  style={styles.camButton}
-                  onPress={() => capturePhoto()}
-                />
-              </View>
-            </>
-          ) : (
-            <>
-              {imageSource !== '' ? (
-                <Image
-                  style={styles.image}
-                  source={{
-                    uri: `file://'${imageSource}`,
-                  }}
-                />
-              ) : null}
-    
-              <View style={styles.backButton}>
-                <TouchableOpacity
-                  style={{
-                    backgroundColor: 'rgba(0,0,0,0.2)',
-                    padding: 10,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: 10,
-                    borderWidth: 2,
-                    borderColor: '#fff',
-                    width: 100,
-                  }}
-                  onPress={() => setShowCamera(true)}>
-                  <Text style={{color: 'white', fontWeight: '500'}}>Back</Text>
-                </TouchableOpacity>
-              </View>
-              <View style={styles.buttonContainer}>
-                <View style={styles.buttons}>
-                  <TouchableOpacity
-                    style={{
-                      backgroundColor: '#fff',
-                      padding: 10,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      borderRadius: 10,
-                      borderWidth: 2,
-                      borderColor: '#77c3ec',
-                    }}
-                    onPress={() => setShowCamera(true)}>
-                    <Text style={{color: '#77c3ec', fontWeight: '500'}}>
-                      Retake
-                    </Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    style={{
-                      backgroundColor: '#77c3ec',
-                      padding: 10,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      borderRadius: 10,
-                      borderWidth: 2,
-                      borderColor: 'white',
-                    }}
-                    onPress={() => setShowCamera(true)}>
-                    <Text style={{color: 'white', fontWeight: '500'}}>
-                      Use Photo
-                    </Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </>
-          )}
+          <Text style={styles.welcome}>Welcome to React Native Mediapipe</Text>
+          <View style={styles.permissionsContainer}>
+            {!hasCameraPermission && (
+              <Text style={styles.permissionText}>
+                React Native Mediapipe needs{' '}
+                <Text style={styles.bold}>Camera permission</Text>.{' '}
+                <Text style={styles.hyperlink} onPress={requestCameraPermission}>
+                  Grant
+                </Text>
+              </Text>
+            )}
+          </View>
         </View>
       );
+    };
+
+
+    return (
+      <SafeAreaView style={styles.root}>
+        {cameraPermission.hasPermission ? (
+          <View style={styles.container}>
+            <MediapipeCamera style={styles.camera} solution={faceDetection} activeCamera='back'/>
+            <Canvas style={styles.overlay}>
+              {segments.map((segment, index) => (
+                <Line
+                  key={index}
+                  p1={vec(segment.startPoint.x, segment.startPoint.y)}
+                  p2={vec(segment.endPoint.x, segment.endPoint.y)}
+                  color="red"
+                  style="stroke"
+                  strokeWidth={2}
+                />
+              ))}
+            </Canvas>
+          </View>
+        ) : (
+          <RequestPermissions
+            hasCameraPermission={cameraPermission.hasPermission}
+            requestCameraPermission={cameraPermission.requestPermission}
+          />
+        )}
+      </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    button: {
-      backgroundColor: 'gray',
-    },
-    backButton: {
-      backgroundColor: 'rgba(0,0,0,0.0)',
-      position: 'absolute',
-      justifyContent: 'center',
-      width: '100%',
-      top: 0,
-      padding: 20,
-    },
-    buttonContainer: {
-      backgroundColor: 'rgba(0,0,0,0.2)',
-      position: 'absolute',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100%',
-      bottom: 0,
-      padding: 20,
-    },
-    buttons: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      width: '100%',
-    },
-    camButton: {
-      height: 80,
-      width: 80,
-      borderRadius: 40,
-      //ADD backgroundColor COLOR GREY
-      backgroundColor: '#B2BEB5',
-  
-      alignSelf: 'center',
-      borderWidth: 4,
-      borderColor: 'white',
-    },
-    image: {
-      width: '100%',
-      height: '100%',
-      aspectRatio: 9 / 16,
-    },
-  });
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'black',
+  },
+  welcome: {color: 'black', fontSize: 38, fontWeight: 'bold', maxWidth: '80%'},
+  banner: {
+    position: 'absolute',
+    opacity: 0.4,
+    bottom: 0,
+    left: 0,
+  },
+  container: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    position: 'relative',
+  },
+  camera: {
+    flex: 1,
+  },
+  overlay: {
+    position: 'absolute',
+     top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  categoriesText: {color: 'black', fontSize: 36},
+  permissionsContainer: {
+    marginTop: 30,
+  },
+  permissionText: {
+    color: 'black',
+    fontSize: 17,
+  },
+  hyperlink: {
+    color: '#007aff',
+    fontWeight: 'bold',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+});
